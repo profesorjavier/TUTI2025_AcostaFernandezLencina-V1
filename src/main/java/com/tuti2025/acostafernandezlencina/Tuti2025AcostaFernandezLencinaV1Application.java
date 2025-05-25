624e8cf96763e2936754e93978bde46fc1a036d2
Francisco Javier Acosta <profesorjaviertecnologia@gmail.com>
2025-05-25 20:05:37 -0300

    prueba inicial

---
+++ src/main/java/com/tuti2025/acostafernandezlencina/Tuti2025AcostaFernandezLencinaV1Application.java
@@ -0,0 +1,13 @@
+package com.tuti2025.acostafernandezlencina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuti2025AcostaFernandezLencinaV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuti2025AcostaFernandezLencinaV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuti2025/acostafernandezlencina/Tuti2025AcostaFernandezLencinaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuti2025.acostafernandezlencina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuti2025AcostaFernandezLencinaV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
