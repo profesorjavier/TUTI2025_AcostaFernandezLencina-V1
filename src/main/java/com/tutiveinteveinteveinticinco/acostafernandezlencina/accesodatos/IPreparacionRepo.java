5d8a360876de37e78397cc4ce9e4d64cacf17bf5
micas <micas@LAPTOP-06DPF7QJ.fibertel.com.ar>
2025-07-14 21:00:41 -0300

    Epic 3

---
+++ src/main/java/com/tutiveinteveinteveinticinco/acostafernandezlencina/accesodatos/IPreparacionRepo.java
@@ -0,0 +1,31 @@
+package com.tutiveinteveinteveinticinco.acostafernandezlencina.accesodatos;
+
+import com.tutiveinteveinteveinticinco.acostafernandezlencina.entidades.Preparacion;
+import com.tutiveinteveinteveinticinco.acostafernandezlencina.entidades.Receta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface IPreparacionRepo extends JpaRepository<Preparacion, Long> {
+
+	@Query("SELECT p FROM Preparacion p JOIN FETCH p.receta WHERE p.activo = true")
+    List<Preparacion> findActivasWithReceta();
+    
+    @Query("SELECT p FROM Preparacion p JOIN FETCH p.receta WHERE p.activo = true AND p.fecha BETWEEN :desde AND :hasta")
+    List<Preparacion> findActivasByFechaWithReceta(@Param("desde") LocalDate desde, @Param("hasta") LocalDate hasta);
+	
+	
+	
+	List<Preparacion> findByActivoTrue();
+
+    Optional<Preparacion> findByFechaAndRecetaAndActivoTrue(LocalDate fecha, Receta receta);
+
+    List<Preparacion> findByFechaBetweenAndActivoTrue(LocalDate desde, LocalDate hasta);
+
+    List<Preparacion> findByReceta_IdAndActivoTrue(Long recetaId);
+}
